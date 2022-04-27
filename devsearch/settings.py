55221d7bf7491e1552b8ff43c2ec53faefe28f69
Abhijeet <abhijeet.a.jain@gmail.com>
2022-04-27 19:36:17 +0530

    settings changed

--- devsearch/settings.py
+++ devsearch/settings.py
@@ -142,7 +142,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'dannydaniels665@gmail.com'
+EMAIL_HOST_USER = 'abhijeetjain@rocketmail.com'
 EMAIL_HOST_PASSWORD = ''
 
 
