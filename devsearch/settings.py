1dfe6bc3bff3da3ee88bcc62c6cd8549d38de330
Abhijeet <abhijeet.a.jain@gmail.com>
2022-04-28 15:08:45 +0530

    changes made to settings.py staticfiles storage

--- devsearch/settings.py
+++ devsearch/settings.py
@@ -177,6 +177,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 # STATIC_ROOT = BASE_DIR / 'static'
 
 # STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE =  'django.contrib.staticfiles.storage.StaticFilesStorage' 
 
 # DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
