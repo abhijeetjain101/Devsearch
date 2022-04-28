08b05d2b9237dec0411d5221f9757c42b53e3b9d
Abhijeet <abhijeet.a.jain@gmail.com>
2022-04-28 16:48:13 +0530

    redo wsgi.py

--- devsearch/wsgi.py
+++ devsearch/wsgi.py
@@ -7,19 +7,19 @@ For more information on this file, see
 https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 """
 
-# import os
-
-# from django.core.wsgi import get_wsgi_application
-
-# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'devsearch.settings')
-
-# application = get_wsgi_application()
-
-
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "devsearch.settings")
 
 from django.core.wsgi import get_wsgi_application
-from whitenoise.django import DjangoWhiteNoise
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'devsearch.settings')
+
 application = get_wsgi_application()
-application = DjangoWhiteNoise(application)
+
+
+# import os
+# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "devsearch.settings")
+
+# from django.core.wsgi import get_wsgi_application
+# from whitenoise.django import DjangoWhiteNoise
+# application = get_wsgi_application()
+# application = DjangoWhiteNoise(application)
