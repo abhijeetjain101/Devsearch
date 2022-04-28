4c6169c6eaccecb98de5ad54db370af48a9fe01e
Abhijeet <abhijeet.a.jain@gmail.com>
2022-04-28 15:05:23 +0530

    changes made to settings.py whitenoise

--- devsearch/settings.py
+++ devsearch/settings.py
@@ -176,7 +176,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 # STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # STATIC_ROOT = BASE_DIR / 'static'
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
